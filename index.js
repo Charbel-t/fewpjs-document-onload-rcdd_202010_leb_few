50bacd07331a89cbc64bdb82c77f9f17148831cc
Charbel t <charbel2000.52@gmail.com>
2020-11-15 16:52:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
 document.addEventListener("DOMContentLoaded",function(){
   document.getElementById('text').textContent='This is really cool!';
 });
+console.log(
+  "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+);
